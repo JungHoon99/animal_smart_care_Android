ba8c1a7fb148dfbc733eed629b9b9802a8ddb255
JungHoon99 <greenjm1@nate.com>
2022-11-05 17:14:12 +0900

    upload android project first

--- app/src/main/java/com/example/animalcare/selectDivice.java
+++ app/src/main/java/com/example/animalcare/selectDivice.java
@@ -47,6 +47,7 @@ public class selectDivice extends AppCompatActivity {
         LinearLayout linear = (LinearLayout) findViewById(R.id.mainLinear);
         FrameLayout f1 = new FrameLayout(this);     //새로운 FrameLayout 생성
         TextView deviceTextView = new TextView(this);
+        TextView petNameTextView = new TextView(this);
 
         LinearLayout.LayoutParams fp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,dpToPx(this,90));
         linear.addView(f1,fp);      //LinearLayout에 FrameLayout 추가
