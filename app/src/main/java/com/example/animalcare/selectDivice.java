6ab0404c4224a37441e2abbe90d150dba6a32e48
JungHoon99 <greenjm1@nate.com>
2022-11-05 18:14:06 +0900

    CreateDeviceLine function and test finish

--- app/src/main/java/com/example/animalcare/selectDivice.java
+++ app/src/main/java/com/example/animalcare/selectDivice.java
@@ -25,30 +25,42 @@ public class selectDivice extends AppCompatActivity {
 
         LinearLayout linear = (LinearLayout) findViewById(R.id.mainLinear); //생성한 LinearLayout 가져오기
 
-        FrameLayout f1 = new FrameLayout(this);     //새로운 FrameLayout 생성
-        f1.setBackgroundColor(Color.parseColor("#77b1e5"));
-        Button btn = new Button(this);      //새로운 버튼 생성
-        btn.setText("Hello");   //버튼에 들어갈 내용을 Hello로 설정
+        CreateDeviceLine("1234","야옹이", 5, "on");
+        CreateDeviceLine("1254","쫑이", 10, "off");
+        CreateDeviceLine("1234","보리", 6, "on");
 
-        /**
-         * LinearLayout에 들어갈 FrameLayout 파라미터 생성 넓이는 부모를 참조하고 높이는 90dp
-         */
-        LinearLayout.LayoutParams fp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,dpToPx(this,90));
-        /**
-         * FrameLayout에 들어갈  Button 파라미터 생성 넓이와 높이 모두 Text에 맞게 설정
-         */
-        FrameLayout.LayoutParams bp = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,FrameLayout.LayoutParams.WRAP_CONTENT);
-        bp.gravity = Gravity.BOTTOM|Gravity.RIGHT;        //버튼 위치를 FrameLayout 오른쪽 아래에 설정
-        f1.addView(btn, bp);        //FrameLayout에 버튼 추가
-        linear.addView(f1,fp);      //LinearLayout에 FrameLayout 추가
     }
 
     public void CreateDeviceLine(String deviceId, String PetName, int PetAge, String State){
         LinearLayout linear = (LinearLayout) findViewById(R.id.mainLinear);
+
         FrameLayout f1 = new FrameLayout(this);     //새로운 FrameLayout 생성
+
         TextView deviceTextView = new TextView(this);
         TextView petNameTextView = new TextView(this);
         TextView petAgeTextView = new TextView(this);
+        Button connectButton = new Button(this);
+
+        deviceTextView.setText(deviceId);
+        petNameTextView.setText(PetName);
+        petAgeTextView.setText(Integer.toString(PetAge));
+        connectButton.setText("connect");
+
+
+        FrameLayout.LayoutParams deviceTextViewParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,FrameLayout.LayoutParams.WRAP_CONTENT);
+        FrameLayout.LayoutParams petNameTextViewParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,FrameLayout.LayoutParams.WRAP_CONTENT);
+        FrameLayout.LayoutParams petAgeTextViewParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,FrameLayout.LayoutParams.WRAP_CONTENT);
+        FrameLayout.LayoutParams connectButtonParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,FrameLayout.LayoutParams.WRAP_CONTENT);
+
+        deviceTextViewParams.gravity = Gravity.LEFT|Gravity.TOP;
+        petNameTextViewParams.gravity = Gravity.LEFT|Gravity.CENTER;
+        petAgeTextViewParams.gravity = Gravity.LEFT|Gravity.BOTTOM;
+        connectButtonParams.gravity  = Gravity.BOTTOM|Gravity.RIGHT;
+
+        f1.addView(deviceTextView,deviceTextViewParams);
+        f1.addView(petNameTextView,petNameTextViewParams);
+        f1.addView(petAgeTextView,petAgeTextViewParams);
+        f1.addView(connectButton,connectButtonParams);
 
         LinearLayout.LayoutParams fp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,dpToPx(this,90));
         linear.addView(f1,fp);      //LinearLayout에 FrameLayout 추가
